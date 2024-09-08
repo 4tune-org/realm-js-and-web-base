ff466329b3f5fe8be21f3808ab9e5553a10d35cf
eXory2024 <exory@anio.software>
2024-09-08 14:57:14 +0200

    Load fortune.config.mjs.

--- src/runtime/generateRuntimeInitData.mjs
+++ src/runtime/generateRuntimeInitData.mjs
@@ -8,6 +8,18 @@ async function loadProjectPackageJSON(project_root) {
 	return JSON.parse(package_json)
 }
 
+async function loadFortuneConfig(project_root) {
+	const fortune_config_path = path.join(project_root, "fortune.config.mjs")
+	const {default: fortune_config} = await import(fortune_config_path)
+	let resolved_config = fortune_config
+
+	if (typeof fortune_config === "function") {
+		resolved_config = await fortune_config()
+	}
+
+	return resolved_config
+}
+
 //
 // Generates the runtime init data needed.
 // This includes:
@@ -18,6 +30,7 @@ export default async function(project_root) {
 	const package_json = await loadProjectPackageJSON(project_root)
 
 	return {
-		package_json
+		package_json,
+		fortune_config: await loadFortuneConfig(project_root)
 	}
 }
