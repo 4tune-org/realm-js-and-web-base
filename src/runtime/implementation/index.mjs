52a6d40cb507437b41dfbe16cc86c44e429cc5ae
eXory2024 <exory@anio.software>
2024-09-08 14:04:50 +0200

    Allow for resources to be passed into initializeRuntimeFromInitData. This is only used for the node runtime capability.

--- src/runtime/implementation/index.mjs
+++ src/runtime/implementation/index.mjs
@@ -1,13 +1,17 @@
-export function initializeRuntimeFromInitData(runtime_init_data) {
-	return {
-		resources: null,
+export function initializeRuntimeFromInitData(
+	runtime_init_data, project_resources = null
+) {
+	const runtime = {
+		resources: project_resources,
 
 		loadProjectPackageJSON() {
 			return JSON.parse(JSON.stringify(runtime_init_data.package_json))
 		},
 
 		loadResource(url) {
-			console.log("load resource", runtime_init_data.resources)
+			console.log("load resource", runtime.resources)
 		}
 	}
+
+	return runtime
 }
